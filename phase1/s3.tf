362b9b6b4f5c6766c6fc736123308f796048e91b
Shishir <shishirraj2912@gmail.com>
2024-03-28 21:11:05 +0530

    s3 bucket creation

---
+++ phase1/s3.tf
@@ -0,0 +1,46 @@
+# provider "aws" {
+#   region = var.my-region # Update with your desired AWS region
+# }
+
+resource "aws_s3_bucket" "my-static-website" {
+  bucket = "my-example-static-website-bucket" # Replace with your desired bucket name
+  #acl    = "public-read"  # Set bucket ACL to allow public read access
+
+  website {
+    index_document = "index.html"  # Set the default index document
+
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.my-static-website.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.my-static-website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.my-static-website.id
+  acl    = "public-read"
+}
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.my-static-website.id
+  key          = "index.html"
+  content_type = "text/html"
+  source       = "${path.module}/index.html"  # Replace with the path to your index.html file
+}
+
--- phase1/variable.tf
+++ phase1/variable.tf
@@ -1,3 +1,11 @@
 variable "alb_name" {
   default = "my-alb"
 }
+
+variable "access_key" {
+  
+}
+
+variable "secret_key" {
+  
+}
