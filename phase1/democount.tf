3a98a2ee99da653ba94c9b9708c1010df9321048
Shishir <shishirraj2912@gmail.com>
2024-04-01 18:40:14 +0530

    count index and conditional expression

--- phase1/democount.tf
+++ phase1/democount.tf
@@ -1,9 +1,30 @@
-resource "aws_instance" "web" {
+# resource "aws_instance" "web" {
+#   ami         ="ami-05295b6e6c790593e"
+#   instance_type = "t2.micro"
+#   count = 3
+#   tags = {
+#     env = var.env_name[count.index]
+#     Name = "myinstance-${count.index+1}"
+#   }
+# }
+
+#conditional expression.. var.varname == true ? number if true:number if false
+
+resource "aws_instance" "dev" {
   ami         ="ami-05295b6e6c790593e"
   instance_type = "t2.micro"
-  count = 3
+  count = var.is_dev_evn == true ? 2:0 #conditional expression
   tags = {
-    env = var.env_name[count.index]
-    Name = "myinstance-${count.index+1}"
+    name = var.instance_name[count.index]    
   }
 }
+
+resource "aws_instance" "prod" {
+  ami         ="ami-05295b6e6c790593e"
+  instance_type = "t2.micro"
+  count = var.is_prod_env == true ? 3:0
+   tags = {
+    name= "prod-${count.index+1}"
+}
+}
+
--- phase1/variable.tf
+++ phase1/variable.tf
@@ -14,3 +14,16 @@ variable "env_name" {
   type = list(string)
   default = ["dev","test","prod"]    
   }
+
+  variable "is_dev_evn" {
+    type = bool
+  }
+  variable "instance_name" {
+    type = list(string)
+    default = [ "devsample","devdemo" ]
+    
+  }
+
+  variable "is_prod_env" {
+    type = bool
+  }
